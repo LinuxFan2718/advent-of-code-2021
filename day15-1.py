ee95e52116e62bf569bb7013b7b49bea5c4d680a
Dennis Cahillane <dennis@denniscahillane.info>
2021-12-18 12:47:05 -0500

    day 15 part 1 correct and fast enough

--- day15-1.py
+++ day15-1.py
@@ -1,8 +1,10 @@
-import random
+import math
 filename = 'input15.txt'
 f = open(filename)
 risk_level_map = []
 visited = []
+dist = []
+prev = []
 while True:
   line = f.readline().strip()
   if line == '':
@@ -10,68 +12,53 @@ while True:
   row = [int(x) for x in list(line)]
   risk_level_map.append(row)
   visited.append([False] * len(row))
+  dist.append([math.inf] * len(row))
+  prev.append([None] * len(row))
 
-total_risk = 0
-x, y = 0, 0
-visited[x][y] = True
+dist[0][0] = 0
+
 destination_x = len(risk_level_map) - 1
 destination_y = len(risk_level_map[0]) - 1
 
-initial_condition = {
-  'position': [0,0],
-  'visited':  visited,
-  'total_risk': 0
-}
-stack = [initial_condition]
-failed = []
-lowest_risk = float('inf')
+while not visited[destination_x][destination_y]:
+  unexplored_vertices = []
+  for i in range(len(dist)):
+    for j in range(len(dist[0])):
+      if not visited[i][j]:
+        unexplored_vertices.append([i, j])
+  least_valued_unexplored_vertex = unexplored_vertices[0]
+  for i in range(1, len(unexplored_vertices)): # find least valued
+    cx, cy = unexplored_vertices[i]
+    lx, ly = least_valued_unexplored_vertex
+    if dist[cx][cy] < dist[lx][ly]:
+      least_valued_unexplored_vertex = unexplored_vertices[i]
+  v = least_valued_unexplored_vertex
+  vx, vy = v
 
-while len(stack) > 0:
-  condition = stack.pop()
-  if condition in failed:
-    continue
-  x, y = condition['position']
-  visited = condition['visited']
-  total_risk = condition['total_risk']
-  if total_risk >= lowest_risk:
-    failed.append(condition)
-    continue
-  
+  visited[vx][vy] = True
   moves = []
-  # if x > 0 and visited[x-1][y] == False: #up
-  #   moves.append([x-1, y])
-  # if y > 0 and visited[x][y-1] == False: # left
-  #   moves.append([x, y -1])
-  if x < len(risk_level_map) - 1 and visited[x+1][y] == False: #down
-    moves.append([x+1, y])
-  if y < len(risk_level_map[0]) - 1 and visited[x][y+1] == False: #right
-    moves.append([x, y+1])
-  if len(moves) == 0:
-    failed.append(condition)
-
+  if vx > 0 and visited[vx-1][vy] == False: #up
+    moves.append([vx-1, vy])
+  if vy > 0 and visited[vx][vy-1] == False: # left
+    moves.append([vx, vy -1])
+  if vx < len(risk_level_map) - 1 and visited[vx+1][vy] == False: #down
+    moves.append([vx+1, vy])
+  if vy < len(risk_level_map[0]) - 1 and visited[vx][vy+1] == False: #right
+    moves.append([vx, vy+1])
   for move in moves:
-    this_x, this_y = move
-    this_visited = [row[:] for row in visited]
-    this_visited[this_x][this_y] = True
-    this_total_risk = total_risk + risk_level_map[this_x][this_y]
-    this_condition = {
-      'position': [this_x, this_y],
-      'visited': this_visited,
-      'total_risk': this_total_risk
-    }
-    if this_x == destination_x and this_y == destination_y and this_total_risk < lowest_risk:
-      lowest_risk = this_total_risk
-      print(f"best solution {this_total_risk}")
-      solution = this_condition
-      for i in range(len(risk_level_map)):
-        for j in range(len(risk_level_map[0])):
-          if this_visited[i][j]:
-            print('\033[91m' + str(risk_level_map[i][j]) + '\033[0m', end = '')
-          else:
-            print(str(risk_level_map[i][j]), end = '')
-        print()
-      print()
-    elif this_total_risk < lowest_risk:
-      stack.append(this_condition)
+    mx, my = move
+    if dist[vx][vy] + risk_level_map[mx][my] < dist[mx][my]:
+      dist[mx][my] = dist[vx][vy] + risk_level_map[mx][my]
+      prev[mx][my] = [vx, vy]
 
-print(f"lowest total risk = {lowest_risk}")
+print(f"lowest total risk = {dist[destination_x][destination_y]}")
+# print(f"best solution {this_total_risk}")
+# solution = this_condition
+# for i in range(len(risk_level_map)):
+#   for j in range(len(risk_level_map[0])):
+#     if this_visited[i][j]:
+#       print('\033[91m' + str(risk_level_map[i][j]) + '\033[0m', end = '')
+#     else:
+#       print(str(risk_level_map[i][j]), end = '')
+#   print()
+# print()
