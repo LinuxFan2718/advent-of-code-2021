2859c8df342ae730526f2ff141b71ffbcfd2e6a4
Dennis Cahillane <dennis@denniscahillane.info>
2021-12-20 14:09:52 -0500

    day 18 skeleton

---
+++ day18-1.py
@@ -0,0 +1,20 @@
+filename = 'input18-test.txt'
+f = open(filename)
+snailfish_numbers = []
+while True:
+  line = f.readline().strip()
+  if line == '':
+    break
+  snailfish_numbers.append(eval(line))
+
+def magnitude(smallfish_number):
+  return None
+
+def split(smallfish_number):
+  return None
+
+def explode(smallfish_number):
+  return None
+
+def addition(smallfish_number1, smallfish_number2):
+  return None
