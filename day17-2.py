62a76f4650f10f10b3c114647a2efe6107932358
Dennis Cahillane <dennis@denniscahillane.info>
2021-12-20 13:51:01 -0500

    day 17 green

--- day17-2.py
+++ day17-2.py
@@ -1,8 +1,8 @@
 DEBUG = True
 VERBOSE = False
 
-input = "target area: x=20..30, y=-10..-5"
-#input = "target area: x=96..125, y=-144..-98"
+#input = "target area: x=20..30, y=-10..-5"
+input = "target area: x=96..125, y=-144..-98"
 
 sub_input = input[13:]
 sub_input_x, sub_input_y = sub_input.split(', ')
@@ -18,13 +18,18 @@ def missed_target(x, y, xr, yb, yt):
   return False
 
 #xv, yv = 15, 0
+import math
 meta_highest_y = 0
 meta_initial = None
 meta_all_initial = []
-for start_xv in range(50):
+meta_xv_small = math.inf
+meta_xv_large = -math.inf
+meta_yv_small = math.inf
+meta_yv_large = -math.inf
+for start_xv in range(150):
   if VERBOSE:
     print(f"x velocity = {start_xv}")
-  for start_yv in range(-100, 100):
+  for start_yv in range(-200, 200):
     x, y = 0, 0
     initial = [start_xv, start_yv]
     highest_y = y
@@ -42,6 +47,14 @@ for start_xv in range(50):
 
       if hit_target(x, y, xl, xr, yb, yt):
         meta_all_initial.append(initial)
+        if start_xv < meta_xv_small:
+          meta_xv_small = start_xv
+        if meta_xv_large < start_xv:
+          meta_xv_large = start_xv
+        if start_yv < meta_yv_small:
+          meta_yv_small = start_yv
+        if meta_yv_large < start_yv:
+          meta_yv_large = start_yv
         if highest_y > meta_highest_y:
           meta_highest_y = highest_y
           meta_initial = initial
@@ -58,3 +71,5 @@ for start_xv in range(50):
 
 print(f"simulation completed. Highest y = {meta_highest_y} with initial x, y = {meta_initial}")
 print(f"num initial velocity found that hit = {len(meta_all_initial)}")
+print(f"xv initial range {meta_xv_small} to {meta_xv_large}")
+print(f"yv initial range {meta_yv_small} to {meta_yv_large}")
