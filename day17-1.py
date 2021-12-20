cfa00c15b7b73fa56eae9c01276fbdd4b53601b1
Dennis Cahillane <dennis@denniscahillane.info>
2021-12-20 13:41:22 -0500

    day 17 part 1 green

--- day17-1.py
+++ day17-1.py
@@ -1,14 +1,13 @@
 DEBUG = True
+VERBOSE = False
 
-input = "target area: x=20..30, y=-10..-5"
-#input = "target area: x=96..125, y=-144..-98"
+#input = "target area: x=20..30, y=-10..-5"
+input = "target area: x=96..125, y=-144..-98"
 
 sub_input = input[13:]
 sub_input_x, sub_input_y = sub_input.split(', ')
 xl, xr = [ int(n) for n in sub_input_x[2:].split('..')]
 yb, yt = [ int(n) for n in sub_input_y[2:].split('..')]
-
-x, y = 0, 0
 
 def hit_target(x, y, xl, xr, yb, yt):
   return xl <= x and x <= xr and yb <= y and y <= yt
@@ -18,26 +17,41 @@ def missed_target(x, y, xr, yb, yt):
     return True
   return False
 
-xv, yv = 9, 0
-initial = [xv, yv]
-while True:
-  x += xv
-  if xv > 0:
-    xv -= 1
-  elif xv < 0:
-    xv += 1
-  y += yv
-  yv -= 1
-  if hit_target(x, y, xl, xr, yb, yt):
-    if DEBUG:
-      print(f"hit target x = {x} y = {y} initial velocity x, y = {initial}")
-    break
-  
-  if missed_target(x, y, xr, yb, yv):
-    if DEBUG:
-      print(f"miss target final position x = {x} y = {y} initial velocity x, y = {initial}")
-    break
-  if DEBUG:
-    print(f"x = {x} y = {y}")
+#xv, yv = 15, 0
+meta_highest_y = 0
+meta_initial = None
+for start_xv in range(20):
+  if VERBOSE:
+    print(f"x velocity = {start_xv}")
+  for start_yv in range(100, 400):
+    x, y = 0, 0
+    initial = [start_xv, start_yv]
+    highest_y = y
+    xv, yv = start_xv, start_yv
+    while True:
+      x += xv
+      if xv > 0:
+        xv -= 1
+      elif xv < 0:
+        xv += 1
+      y += yv
+      yv -= 1
+      if y > highest_y:
+        highest_y = y
 
-print(f"simulation completed")
+      if hit_target(x, y, xl, xr, yb, yt):
+        if highest_y > meta_highest_y:
+          meta_highest_y = highest_y
+          meta_initial = initial
+        if DEBUG:
+          print(f"hit target x = {x} y = {y} initial velocity x, y = {initial} final v = {xv},{yv} highest y = {highest_y}")
+        break
+      
+      if missed_target(x, y, xr, yb, yv):
+        if VERBOSE:
+          print(f"miss target final position x = {x} y = {y} initial velocity x, y = {initial}")
+        break
+      if VERBOSE:
+        print(f"x = {x} y = {y}")
+
+print(f"simulation completed. Highest y = {meta_highest_y} with initial x, y = {meta_initial}")
