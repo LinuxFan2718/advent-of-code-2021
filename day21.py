3002f530e60fb0b7923facb31d40399d826d14e6
Dennis Cahillane <dennis@thirtymadison.com>
2021-12-23 01:56:25 -0500

    day 21 part 2 counting too small

--- day21.py
+++ day21.py
@@ -1,49 +1,44 @@
-import sys
-DEBUG = True
-position = [None, 4, 8]
-score = [None, 0, 0]
-rolls = 0
+winning_score = 21
+position = [4, 8]
+score = [0, 0]
+game = {
+  "position": position,
+  "score": score,
+  "player": 0,
+  "occurances": 1
+}
+wins = [0 ,0]
+games = [game]
+rolls = {
+  3: 1, 
+  4: 3,
+  5: 6,
+  6: 7,
+  7: 6, 
+  8: 3, 
+  9: 1
+}
 
-dice_sides = 100
-def dice_roll():
-  roll = 1
-  while True:
-    yield roll
-    roll += 1
-    if roll > 100:
-      roll = 1
+while len(games) > 0:
+  new_games = []
+  for game in games:
+    player = game["player"]
+    for roll in rolls:
+      new_game = game.copy()
+      num_occurances = rolls[roll]
+      current_position = new_game["position"][player]
+      current_position += roll
+      if current_position > 10:
+        current_position -= 10
+      
+      new_game["position"][player] = current_position
+      new_game["score"][player] += current_position
+      new_game["player"] = (player + 1) % 2
+      new_game["occurances"] *= num_occurances
 
-die = dice_roll()
-
-def report():
-  for i in [1,2]:
-    print(f"player {i} position {position[i]} score {score[i]} score*rolls = {score[i]*rolls}")
-  print(f"rolls {rolls}")
-  sys.exit()
-
-def turn(player):
-  global rolls
-  rolls += 3
-
-  roll = [next(die), next(die), next(die)]
-  if DEBUG:
-    print(f"player {player} rolls {roll}")
-  result = sum(roll)
-  adjusted_result = result % 10
-  current_position = position[player]
-  current_position += adjusted_result
-  if current_position > 10:
-    current_position -= 10
-  position[player] = current_position
-  if DEBUG:
-    print(f"player {player} current position {current_position}")
-  score[player] += current_position
-  if DEBUG:
-    print(f"player {player} current score {score[player]}")
-  if score[player] >= 1000:
-    print(f"GAME OVER player {player} won with score {score[player]}")
-    report()
-  
-while True:
-  turn(1)
-  turn(2)
+      if new_game["score"][player] >= winning_score:
+        wins[player] += new_game["occurances"]
+      else:
+        new_games.append(new_game)
+  games = new_games
+print(wins)
